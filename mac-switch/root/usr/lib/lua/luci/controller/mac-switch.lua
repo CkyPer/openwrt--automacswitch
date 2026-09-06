32745e82134ff7e653edb4de1f581705928a7730
CkyPer <121272979+CkyPer@users.noreply.github.com>
2026-09-06 16:29:15 +0800

    Add MAC switch controller to LuCI

--- mac-switch/root/usr/lib/lua/luci/controller/mac-switch.lua
+++ mac-switch/root/usr/lib/lua/luci/controller/mac-switch.lua
@@ -1,1 +1,10 @@
+module("luci.controller.mac-switch", package.seeall)
 
+function index()
+    if not nixio.fs.access("/etc/config/mac-switch") then
+        return
+    end
+
+    entry({"admin", "system", "mac-switch"}, alias("admin", "system", "mac-switch", "general"), _("MAC Auto Switch"), 60).dependent = true
+    entry({"admin", "system", "mac-switch", "general"}, cbi("mac-switch"), _("Settings"), 10)
+end
